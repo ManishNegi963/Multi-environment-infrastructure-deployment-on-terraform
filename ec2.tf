eba57b7341bd21425d8092284c3d02b1c0b2c99f
ManishNegi963 <124788172+ManishNegi963@users.noreply.github.com>
2023-09-14 17:42:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "my-demo-instance" {
                 ami = var.ami_id
                 instance_type = var.instance_type
                 tags = {
-                        Name = "${var.env_name}_${var.instance_name}"
+                        Name = "${var.env_name}-${var.instance_name}"
         }
 }
